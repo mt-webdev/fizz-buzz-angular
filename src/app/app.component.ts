a2c45b9722e4caf52590c69194049a6b3db4fc92
mtwd <mt-webdev@outlook.com>
2024-05-24 15:50:38 +0200

    stop button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { FizzBuzzService } from './services/fizz-buzz.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
   providers: [FizzBuzzService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'fizz-buzz-angular';
+  private formBuilder = inject(FormBuilder);
+  formGroup = this.formBuilder.group({
+    input: ['', [Validators.required, Validators.pattern('stop')]],
+  });
 }
